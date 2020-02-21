d851fb1c138077a94e4f7bc1d4a550946f7f0240
Shafrom <shafrom108@gmail.com>
2020-02-22 00:24:01 +0300

    add new strings to js code

--- js/script.js
+++ js/script.js
@@ -11,4 +11,20 @@ new Swiper('.ourResources__slider', {
     type: 'bullets',
     clickable: 1
   },
-})
+});
+
+new Swiper('.testimonials__slider', {
+  mousewheel: true,
+  loop: true,
+  slidesPerView: 1.7,
+  spaceBetween: 64,
+  // navigation: {
+  //   nextEl: '.arrow__pic-right',
+  //   prevEl: '.arrow__pic-left',
+  // },
+  pagination: {
+    el: '.testimonial__pagination',
+    type: 'bullets',
+    clickable: 1
+  },
+});
